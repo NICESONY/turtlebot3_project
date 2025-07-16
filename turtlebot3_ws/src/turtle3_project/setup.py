e6dbc00a0aafd58dfde6ec66ddae71e427e63f8a
NICESONY <songunhee5426@gmail.com>
2025-07-16 21:14:07 +0900

    Enhance setup.py to include additional data files and entry points; add test_node.py

--- turtlebot3_ws/src/turtle3_project/setup.py
+++ turtlebot3_ws/src/turtle3_project/setup.py
@@ -1,5 +1,7 @@
 from setuptools import find_packages, setup
-
+import os 
+import sys
+from glob import glob
 package_name = 'turtle3_project'
 
 setup(
@@ -7,9 +9,35 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        # 패키지 등록용
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+
+        # 패키지 XML 파일
         ('share/' + package_name, ['package.xml']),
+    
+        # launch 폴더 안의 모든 .launch.py 파일 
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')
+        ),
+        # maps 폴더 안의 모든 .yaml 파일
+        (
+            os.path.join('share', package_name, 'maps'),
+            glob('maps/*.*')
+        ),
+        # config  폴더 안의 모든 .yaml 파일
+        (
+            os.path.join('share', package_name, 'config'),
+            glob('config/*.yaml')
+        ),
+        # worlds folder .world file
+                (
+            os.path.join('share', package_name, 'worlds'),
+            glob('worlds/*.world')
+        ),
+
+    
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +48,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'test_node = turtle3_project.test_node:main',
         ],
     },
 )
