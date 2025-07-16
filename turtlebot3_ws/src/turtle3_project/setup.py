4aa71241e9cc080d4caf4b0db490e8a337b5b64f
NICESONY <songunhee5426@gmail.com>
2025-07-16 21:41:31 +0900

    update

--- turtlebot3_ws/src/turtle3_project/setup.py
+++ turtlebot3_ws/src/turtle3_project/setup.py
@@ -54,6 +54,7 @@ setup(
             'turtlebot3_robot_video = turtle3_project.turtlebot3_robot_video:main',
             'turtlebot3_sim_video = turtle3_project.turtlebot3_sim_video:main',
             'logic_flow = turtle3_project.logic_flow:main',
+            
         ],
     },
 )
