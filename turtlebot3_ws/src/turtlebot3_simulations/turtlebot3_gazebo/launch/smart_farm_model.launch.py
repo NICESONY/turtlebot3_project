3971ed80f3ef59934f4d28f2067c37d54673488c
NICESONY <songunhee5426@gmail.com>
2025-07-22 13:06:02 +0900

    update

--- turtlebot3_ws/src/turtlebot3_simulations/turtlebot3_gazebo/launch/smart_farm_model.launch.py
+++ turtlebot3_ws/src/turtlebot3_simulations/turtlebot3_gazebo/launch/smart_farm_model.launch.py
@@ -25,7 +25,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 
-def generate_launch_description():
+def generate_launch_description() :
     launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
