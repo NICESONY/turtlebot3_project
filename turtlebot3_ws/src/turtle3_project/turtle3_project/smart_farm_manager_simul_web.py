521fbb06a52fc29bf37451f34edc259fb6f94f00
NICESONY <songunhee5426@gmail.com>
2025-07-24 13:14:32 +0900

    update

--- turtlebot3_ws/src/turtle3_project/turtle3_project/smart_farm_manager_simul_web.py
+++ turtlebot3_ws/src/turtle3_project/turtle3_project/smart_farm_manager_simul_web.py
@@ -14,7 +14,7 @@ ros2 launch turtlebot3_gazebo smart_farm_model.launch.py
 
 ros2 launch turtlebot3_navigation2 smart_farm_navigation2_simul.launch.py use_sim_time:=true
 
-ros2 run turtle3_project smart_farm_manager_simul
+ros2 run turtle3_project smart_farm_manager_simul_web
 
 
 debugging code
