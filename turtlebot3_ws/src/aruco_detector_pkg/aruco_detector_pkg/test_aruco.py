572d41f735bbeaeeb3464b6b7fedb5f9a9524828
jinuk9 <wlsdnsla2568@gmail.com>
2025-07-24 11:09:42 +0900

    Update test_aruco.py

--- turtlebot3_ws/src/aruco_detector_pkg/aruco_detector_pkg/test_aruco.py
+++ turtlebot3_ws/src/aruco_detector_pkg/aruco_detector_pkg/test_aruco.py
@@ -12,6 +12,64 @@ from sensor_msgs.msg import CompressedImage
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger
 
+
+
+"""
+
+### robot
+
+```
+
+
+mkdir root_ws
+cd root_ws
+mkdir src
+cd src
+ros2 pkg create tab_pkg --build_type ament_python
+
+# 터틀봇 전반적인 기능 실행
+ros2 launch turtlebot3_bringup robot.launch.py
+
+# 카메라 실행
+ros2 run v4l2_camera v4l2_camera_node \
+  --ros-args -r __ns:=/camera \
+            -p image_size:="[640,480]"
+            
+
+ ros2 run tab_pkg manual_dump_service_server 
+
+```
+
+
+## local
+
+
+```
+mkdir local_ws
+cd local_ws
+mkdir src
+cd src
+ros2 pkg create aruco_detector_pkg --build_type ament_python
+
+ros2 run aruco_detector_pkg test_aruco
+
+```
+
+
+## do you want to show image/?????????
+
+
+```
+rqt
+
+```
+
+
+
+"""
+
+
+
 class CropHarvestNode(Node):
     def __init__(self):
         super().__init__('crop_harvest_node')
